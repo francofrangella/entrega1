e15fc89bd8e96f482bf0724cb4e276a15ea18b23
francofrangella <ffangella25@gmail.com>
2023-12-11 14:56:40 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
+    constructor(filePath) {
+        this.path = filePath;
         this.products = [];
-        this.productIdCounter = 1; 
+        this.productIdCounter = 1;
+
+    
+        this.loadProducts();
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
- 
+    addProduct(productData) {
+        const { title, description, price, thumbnail, code, stock } = productData;
+
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.error("Todos los campos son obligatorios.");
             return;
@@ -17,7 +24,6 @@ class ProductManager {
             return;
         }
 
-
         const newProduct = {
             id: this.productIdCounter++,
             title,
@@ -27,7 +33,9 @@ class ProductManager {
             code,
             stock
         };
+
         this.products.push(newProduct);
+        this.saveProducts();
 
         return newProduct;
     }
@@ -45,17 +53,74 @@ class ProductManager {
             return null;
         }
     }
+
+    updateProduct(id, field, value) {
+        const productIndex = this.products.findIndex(product => product.id === id);
+        if (productIndex !== -1) {
+            this.products[productIndex][field] = value;
+            this.saveProducts();
+        } else {
+            console.error("Producto no encontrado.");
+        }
+    }
+
+    deleteProduct(id) {
+        const productIndex = this.products.findIndex(product => product.id === id);
+        if (productIndex !== -1) {
+            this.products.splice(productIndex, 1);
+            this.saveProducts();
+        } else {
+            console.error("Producto no encontrado.");
+        }
+    }
+
+    loadProducts() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf8');
+            this.products = JSON.parse(data);
+            this.productIdCounter = Math.max(...this.products.map(product => product.id), 0) + 1;
+        } catch (error) {
+            console.error("Error al cargar productos desde el archivo:", error.message);
+        }
+    }
+
+    saveProducts() {
+        try {
+            const data = JSON.stringify(this.products, null, 2);
+            fs.writeFileSync(this.path, data, 'utf8');
+        } catch (error) {
+            console.error("Error al guardar productos en el archivo:", error.message);
+        }
+    }
 }
 
+const manager = new ProductManager('productos.json');
 
-const manager = new ProductManager();
+manager.addProduct({
+    title: "Producto 1",
+    description: "Descripción 1",
+    price: 20.5,
+    thumbnail: "imagen1.jpg",
+    code: "P001",
+    stock: 100
+});
 
-manager.addProduct("Producto 1", "Descripción 1", 20.5, "imagen1.jpg", "P001", 100);
-manager.addProduct("Producto 2", "Descripción 2", 30.0, "imagen2.jpg", "P002", 50);
+manager.addProduct({
+    title: "Producto 2",
+    description: "Descripción 2",
+    price: 30.0,
+    thumbnail: "imagen2.jpg",
+    code: "P002",
+    stock: 50
+});
 
 console.log(manager.getProducts());
 
 const foundProduct = manager.getProductById(1);
 console.log(foundProduct);
 
-const notFoundProduct = manager.getProductById(3);
+manager.updateProduct(1, 'price', 25.0);
+console.log(manager.getProducts());
+
+manager.deleteProduct(1);
+console.log(manager.getProducts());
